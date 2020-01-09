a0a45ffd42a4c2035a42d9f272f2e3161e813fdd
UttkarshJha <uttkarshjha1501@gmail.com>
2020-01-09 23:01:12 +0530

    checking how commit works

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("hello world from master branch!!");
+		System.out.println("checking how commit works");
 		System.exit(0);
 	}
 }
