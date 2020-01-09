75c26b8cc12ad072be18f570aba84e965080e38f
UttkarshJha <uttkarshjha1501@gmail.com>
2020-01-09 23:28:26 +0530

    sahil

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world from master branch!!");
-		System.out.println("checking how commit works");
+		System.out.println("hello world from sahil branch!!");
+		//System.out.println("checking how commit works");
 		System.exit(0);
 	}
 }
